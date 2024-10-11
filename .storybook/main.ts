789b67dd87dcc4fce2e536fefb3a883cb51fecce
LilyStilson <lunamusicoff@gmail.com>
2024-10-11 14:19:29 +0200

    fix: removed jsdoc-to-mdx plugin

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -7,14 +7,7 @@ const config: StorybookConfig = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@chromatic-com/storybook",
-    "@storybook/addon-interactions",
-    {
-        name: 'storybook-addon-jsdoc-to-mdx',
-        options: {
-            folderPaths: ['./src/'], 
-            extensions: ['ts', 'js', 'tsx', 'jsx']
-        }
-    }
+    "@storybook/addon-interactions"
   ],
   framework: {
     name: "@storybook/react-vite",
